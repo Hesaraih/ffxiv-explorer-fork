e1f4bf7b39faf9d4745277e90f47c7b5f08f5fb5
Hesaraih <hesaraih@hotmail.com>
2021-05-24 14:13:45 +0900

    avfxファイルからHashDBへ自動登録できるようにした

--- src/main/java/com/fragmenterworks/ffxivextract/models/AVFX_File.java
+++ src/main/java/com/fragmenterworks/ffxivextract/models/AVFX_File.java
@@ -1,26 +1,38 @@
 package com.fragmenterworks.ffxivextract.models;
 
+import com.fragmenterworks.ffxivextract.Constants;
 import com.fragmenterworks.ffxivextract.helpers.Utils;
+import com.fragmenterworks.ffxivextract.storage.HashDatabase;
 
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class AVFX_File extends Game_File {
 
+    //他のファイルを見つけるために使用されます
+    private final SqPack_IndexFile currentIndex;
+
     private final int fileSize;
     private final ArrayList<AVFX_Packet> packets = new ArrayList<>();
 
-    public AVFX_File(byte[] data, ByteOrder endian) {
+    public AVFX_File(SqPack_IndexFile index, byte[] data, ByteOrder endian) {
         super(endian);
         ByteBuffer bb = ByteBuffer.wrap(data);
         bb.order(endian);
+        currentIndex = index;
 
-        bb.getInt(); //Signature
-        fileSize = bb.getInt(); //File Size
+        String sig = getString(bb,4); //ファイルシグネチャ(reverseでAVFX)
+        if (sig.equals("XFVA")) {
+            fileSize = bb.getInt(); //シグネチャとサイズデータ(int)を除いたデータサイズ
 
-        while (bb.hasRemaining()) {
-            packets.add(new AVFX_Packet(bb));
+            while (bb.hasRemaining()) {
+                packets.add(new AVFX_Packet(bb));
+            }
+        }else{
+            fileSize = bb.capacity();
         }
     }
 
@@ -34,17 +46,24 @@ public class AVFX_File extends Game_File {
         final byte[] data;
 
         AVFX_Packet(ByteBuffer inBuff) {
-            inBuff.get(tag);
+            inBuff.get(tag);  //データタイプシグネチャ(xeT ,reV ,PFDbなど)
 
-            dataSize = inBuff.getInt();
+            dataSize = inBuff.getInt(); //シグネチャとサイズデータ(int)を除いたデータサイズ
+            if(dataSize > inBuff.limit()){
+                //なんらかのエラーが発生してる時ここに誘導
+                data = new byte[0];
+                return;
+            }
 
-            //文字列のデータサイズはすべてめちゃくちゃです
+            //文字列のデータサイズはすべて違います
             if (tag[0] == 0x78 && tag[1] == 0x65 && tag[2] == 0x54) {
+                //xeT :テキスト格納ブロックのシグネチャ(reverseでTex)
                 int increment = 0;
                 int curPos = inBuff.position();
-                inBuff.position(curPos + dataSize);
+                inBuff.position(curPos + dataSize); //byteバッファの読み込み開始アドレスをデータサイズ分進める
 
                 while (inBuff.hasRemaining() && inBuff.get() == 0x0) {
+                    //文字列末尾の0x00データを検索
                     increment++;
                 }
 
@@ -72,8 +91,87 @@ public class AVFX_File extends Game_File {
         }
     }
 
+    /**
+     * AVFXファイル中のパス文字列をHashデータベースに登録する
+     */
+    public void regHash() {
+        regHash(false);
+    }
+
+    /**
+     * AVFXファイル中のパス文字列をHashデータベースに登録する
+     * @param fileCheck true:ファイルチェックをする
+     */
+    public void regHash(boolean fileCheck) {
+
+        SqPack_IndexFile index = null;
+
+        HashDatabase.beginConnection();
+        try {
+            HashDatabase.setAutoCommit(false);
+        } catch (SQLException e1) {
+            Utils.getGlobalLogger().error(e1);
+        }
+
+        for (AVFX_Packet ap : packets){
+            String apTag = new StringBuffer(new String(ap.tag)).reverse().toString().trim();
+            if (apTag.equals("Tex")){
+                String fullPath = new String(ap.data).trim();
+                String archive = HashDatabase.getArchiveID(fullPath);
+                if (fileCheck) {
+                    int pathCheck = 0;
+                    if (currentIndex.getName().equals(archive)){
+                        pathCheck = currentIndex.existsFile2(fullPath);
+                    }else {
+                        try {
+                            if (index == null || !index.getName().equals(archive)) {
+                                index = new SqPack_IndexFile(Constants.datPath + "\\game\\sqpack\\ffxiv\\" + archive + ".win32.index", true);
+                            }
+                            pathCheck = index.existsFile2(fullPath);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (pathCheck == 2) {
+                        HashDatabase.addPathToDB(fullPath, archive);
+                    } else if (pathCheck == 1) {
+                        //ファイルパスのみ追加
+                        String folder;
+                        if (fullPath.contains(".")) {
+                            folder = fullPath.substring(0, fullPath.lastIndexOf("/"));
+                        } else {
+                            folder = fullPath;
+                        }
+
+                        HashDatabase.addFolderToDB(folder, archive);
+                    }
+                }else{
+                    HashDatabase.addPathToDB(fullPath, archive);
+                }
+            }
+        }
+
+        try {
+            HashDatabase.commit();
+        } catch (SQLException e) {
+            Utils.getGlobalLogger().error(e);
+        }
+        HashDatabase.closeConnection();
+    }
     public void printOut() {
         packets.forEach(ap -> Utils.getGlobalLogger().trace(ap));
     }
 
+    /**
+     * Convenience function for reading a fixed length string from a ByteBuffer
+     *
+     * @param buffer     The buffer to read from
+     * @param byteLength The number of bytes to read
+     * @return A new non-trimmed string read from the given buffer with the given length.
+     */
+    private static String getString(ByteBuffer buffer, int byteLength) {
+        byte[] input = new byte[byteLength];
+        buffer.get(input);
+        return new String(input);
+    }
 }
