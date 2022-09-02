3abf44df0291c7c49861d392ab059b084c35a0d6
Hesaraih <hesaraih@hotmail.com>
2022-09-02 13:56:26 +0900

    GraphicsNodeTypeData_5があるらしいので追加

---
+++ src/main/java/com/fragmenterworks/ffxivextract/models/uldStuff/GraphicsNodeTypeData_5.java
@@ -0,0 +1,20 @@
+package com.fragmenterworks.ffxivextract.models.uldStuff;
+
+import java.nio.ByteBuffer;
+
+/**
+ * Created by Roze on 2017-06-23.
+ *
+ * @author Roze
+ */
+public class GraphicsNodeTypeData_5 extends GraphicsNodeTypeData {
+
+    public GraphicsNodeTypeData_5(final ByteBuffer data) {
+        super(data);
+    }
+
+    @Override
+    public String toString() {
+        return "GraphicsNodeTypeData_5{} " + super.toString();
+    }
+}
